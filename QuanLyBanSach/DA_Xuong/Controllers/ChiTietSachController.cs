ff8dfb7cf8dd88158da99b939c72d0ddfb65f54e
Hennry20 <145514814+Hennry20@users.noreply.github.com>
2024-03-26 14:44:04 +0700

    Chi tiết sách
    Form Chi Tiết Sách (chưa xong)

---
+++ QuanLyBanSach/DA_Xuong/Controllers/ChiTietSachController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DA_Xuong.Controllers
+{
+    public class ChiTietSachController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
