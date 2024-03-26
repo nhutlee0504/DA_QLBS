44e529f1dd66ad616be258e4c2c4e5f58215ca5b
nhutlee0504 <leminhnhut718@gmail.com>
2024-03-26 14:13:50 +0700

    Commit Lần Thứ 1 (Hoàn thành form sale)
    Đã hoàn thành giao diện trang Sale

---
+++ QuanLyBanSach/DA_Xuong/Controllers/SaleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DA_Xuong.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
